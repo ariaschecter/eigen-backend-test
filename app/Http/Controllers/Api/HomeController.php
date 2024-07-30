e4f341dd3b16730c0cdc5326b95f475be31b6253
Arlx <aria.maulana.m@gmail.com>
2024-07-30 20:48:16 +0700

    feat: implement swagger api documentation

---
+++ app/Http/Controllers/Api/HomeController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+/**
+ * @OA\Get(
+ *     path="/home",
+ *     tags={"Users"},
+ *     summary="Get list of users",
+ *     description="Returns list of users",
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful operation",
+ *         @OA\JsonContent(
+ *             type="array",
+ *             @OA\Items(ref="#/components/schemas/User")
+ *         ),
+ *     ),
+ *     @OA\Response(
+ *         response=400,
+ *         description="Bad request",
+ *     )
+ * )
+ */
+
+class HomeController extends Controller
+{
+    /**
+     * @OA\Schema(
+     *     schema="User",
+     *     type="object",
+     *     title="User",
+     *     required={"name", "email"},
+     *     @OA\Property(
+     *         property="id",
+     *         type="string",
+     *     ),
+     *     @OA\Property(
+     *         property="name",
+     *         type="string",
+     *         example="John Doe"
+     *     ),
+     *     @OA\Property(
+     *         property="email",
+     *         type="string",
+     *         format="email",
+     *         example="johndoe@example.com"
+     *     ),
+     * )
+     */
+    public function index()
+    {
+        return response()->json([
+            'data' => User::all()
+        ]);
+    }
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,33 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="My API",
+ *     description="API Documentation",
+ *     @OA\Contact(
+ *         email="support@example.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ * @OA\Server(
+ *     url="https://eigen-test.acielana.my.id/api",
+ *     description="DevelopmentServer"
+ * )
+ * @OA\Server(
+ *     url="http://127.0.0.1:8000/api",
+ *     description="Local Server"
+ * )
+ */
+class SwaggerController extends BaseController
+{
+    // You can place additional annotations here
+}
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
