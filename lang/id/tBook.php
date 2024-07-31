a0fb9879e69079ff02fc7267f8729e851de5a14a
Arlx <aria.maulana.m@gmail.com>
2024-07-31 22:34:42 +0700

    feat: tBook success failed status

--- lang/id/tBook.php
+++ lang/id/tBook.php
@@ -3,5 +3,10 @@
 return [
     "success" => [
         "return" => "Berhasil Mengembalikan Buku."
+    ],
+    "failed"  => [
+        "member_penalty" => "Member sedang mengalami penalty.",
+        "member_quota"   => "Member telah mencapai batas kuota pinjam buku.",
+        "book_quota"     => "Buku habis."
     ]
 ];
